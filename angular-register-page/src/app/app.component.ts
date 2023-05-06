66d599025869fe29936f60b3958e80491d67e14b
lelethu <lelethumlinjana71@gmail.com>
2023-05-06 11:30:26 +0200

    messsage

--- angular-register-page/src/app/app.component.ts
+++ angular-register-page/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-register-page';
+export class AppComponent implements OnInit{
+  myForm: FormGroup;
+  // title = 'angular-register-page';
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.maxLength(15), Validators.minLength(4)]],
+      verifypassword: ['', [Validators.required, Validators.maxLength(15), Validators.minLength(4)]],
+     message: ['', [Validators.required, Validators.minLength(15)]],
+    });
+
+  }
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid); // true or false
+    console.log('Name', form.value.name);
+    console.log('Email', form.value.email);
+    console.log('Password', form.value.password);
+    console.log('VerifyPassword', form.value.VerifyPassword);
+    console.log('Message', form.value.message);
+  }
+
 }
